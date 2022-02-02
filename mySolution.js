ac4b498c2936c90c2782212a37b64e16173f8ed0
Anton Drevetskyi <drevetskyianton@gmail.com>
2022-02-02 16:42:52 -0700

    "my sol"

--- mySolution.js
+++ mySolution.js
@@ -13,3 +13,26 @@
 //   console.log(result);
 //   return Number(result);
 // }
+
+//Highest and Lowest
+// function highAndLow(numbers) {
+//   const array = numbers.split(" ");
+//   let minNum = +array[0];
+//   let maxNum = +array[0];
+//   for (let item of array) {
+//     if (+item < minNum) minNum = +item;
+//     if (+item > maxNum) maxNum = +item;
+//   }
+//   return `${maxNum} ${minNum}`;
+// }
+
+// Square Every Digit
+// function squareDigits(num) {
+//   let numToString = String(num);
+//   let result = "";
+//   for (let i = 0; i<numToString.length; i++ ) {
+//     let sqe = parseInt(numToString[i]) * parseInt(numToString[i]);
+//     result = result + String(sqe);
+//   }
+//   return parseInt(result);
+// }
