a95434eff18454c2848840ea1ade92a427f00c4d
Anton Drevetskyi <drevetskyianton@gmail.com>
2022-02-02 16:41:27 -0700

    "init"

---
+++ mySolution.js
@@ -0,0 +1,15 @@
+// Mumbling
+// function accum(s) {
+//   let result = [...s]
+//     .map((char, index) => char.toUpperCase() + char.toLowerCase().repeat(index))
+//     .join("-");
+//   return result;
+// }
+
+// Descending Order
+// function descendingOrder(n) {
+//   let array = String(n).split("");
+//   let result = array.sort().reverse().join("");
+//   console.log(result);
+//   return Number(result);
+// }
